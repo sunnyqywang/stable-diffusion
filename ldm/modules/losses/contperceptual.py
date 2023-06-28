0ebfabc9767198a7a5f02fa7bacf554c7c8fcd55
sunnyqywang <sunnywang950529@hotmail.com>
2023-06-27 20:49:42 -0400

    Add SAE

--- ldm/modules/losses/contperceptual.py
+++ ldm/modules/losses/contperceptual.py
@@ -8,7 +8,7 @@ class LPIPSWithDiscriminator(nn.Module):
     def __init__(self, disc_start, logvar_init=0.0, kl_weight=1.0, pixelloss_weight=1.0,
                  disc_num_layers=3, disc_in_channels=3, disc_factor=1.0, disc_weight=1.0,
                  perceptual_weight=1.0, use_actnorm=False, disc_conditional=False,
-                 disc_loss="hinge"):
+                 disc_loss="hinge", supervision_weight=1.0):
 
         super().__init__()
         assert disc_loss in ["hinge", "vanilla"]
@@ -28,6 +28,7 @@ class LPIPSWithDiscriminator(nn.Module):
         self.disc_factor = disc_factor
         self.discriminator_weight = disc_weight
         self.disc_conditional = disc_conditional
+        self.supervision_weight = supervision_weight
 
     def calculate_adaptive_weight(self, nll_loss, g_loss, last_layer=None):
         if last_layer is not None:
@@ -42,8 +43,17 @@ class LPIPSWithDiscriminator(nn.Module):
         d_weight = d_weight * self.discriminator_weight
         return d_weight
 
+    def calculate_adaptive_weight2(self, nll_loss, g_loss, last_layer1=None, last_layer2=None):
+        g_grads = torch.autograd.grad(g_loss, last_layer2, retain_graph=True)[0]
+        nll_grads = torch.autograd.grad(nll_loss, last_layer1, retain_graph=True)[0]
+
+        d_weight = torch.norm(nll_grads) / (torch.norm(g_grads) + 1e-4)
+        d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
+        d_weight = d_weight * self.discriminator_weight
+        return d_weight
+
     def forward(self, inputs, reconstructions, posteriors, optimizer_idx,
-                global_step, last_layer=None, cond=None, split="train",
+                global_step, supervisions=None, last_layer=None, supervisor_layer=None, cond=None, split="train",
                 weights=None):
         image_dim = reconstructions.shape[1]
         inputs = inputs[:,:image_dim,:,:]
@@ -61,6 +71,20 @@ class LPIPSWithDiscriminator(nn.Module):
         kl_loss = posteriors.kl()
         kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
 
+        if supervisions is not None:
+            s_loss = torch.abs(supervisions[0].contiguous()-supervisions[1].contiguous())
+            s_loss = torch.sum(s_loss) / s_loss.shape[0]
+
+            assert supervisor_layer is not None
+            try:
+                s_weight = self.calculate_adaptive_weight2(nll_loss, s_loss,
+                     last_layer1=last_layer, last_layer2=supervisor_layer)
+            except:
+                s_weight=torch.tensor(0.0)
+        else:
+            s_weight = 0
+            s_loss = 0
+
         # now the GAN part
         if optimizer_idx == 0:
             # generator update
@@ -75,6 +99,8 @@ class LPIPSWithDiscriminator(nn.Module):
             if self.disc_factor > 0.0:
                 try:
                     d_weight = self.calculate_adaptive_weight(nll_loss, g_loss, last_layer=last_layer)
+                    # print(d_weight)
+                    # nll_grads = torch.autograd.grad(nll_loss, last_layer, retain_graph=True)[0]
                 except RuntimeError:
                     assert not self.training
                     d_weight = torch.tensor(0.0)
@@ -82,7 +108,9 @@ class LPIPSWithDiscriminator(nn.Module):
                 d_weight = torch.tensor(0.0)
 
             disc_factor = adopt_weight(self.disc_factor, global_step, threshold=self.discriminator_iter_start)
-            loss = weighted_nll_loss + self.kl_weight * kl_loss + d_weight * disc_factor * g_loss
+            loss = weighted_nll_loss + self.kl_weight * kl_loss + \
+                   d_weight * disc_factor * g_loss + \
+                   s_weight * s_loss
 
             log = {"{}/total_loss".format(split): loss.clone().detach().mean(), "{}/logvar".format(split): self.logvar.detach(),
                    "{}/kl_loss".format(split): kl_loss.detach().mean(), "{}/nll_loss".format(split): nll_loss.detach().mean(),
@@ -90,6 +118,8 @@ class LPIPSWithDiscriminator(nn.Module):
                    "{}/d_weight".format(split): d_weight.detach(),
                    "{}/disc_factor".format(split): torch.tensor(disc_factor),
                    "{}/g_loss".format(split): g_loss.detach().mean(),
+                   "{}/s_weight".format(split): s_weight.detach().mean(),
+                   "{}/s_loss".format(split): s_loss.detach().mean(),
                    }
             return loss, log
 
